e23386adbdf166b395eefef17f9663dc324b71df
Himanshu Bhindoriya (HB) <frontifybyhb@gmail.com>
2025-11-19 15:48:39 +0530

    Change API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000/api'; // Update this with your backend URL
+const baseURL = 'https://emp-backend-8jrr.onrender.com/api'; // Update this with your backend URL
 
 const api = axios.create({
     baseURL,
